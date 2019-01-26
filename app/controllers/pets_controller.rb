1f52dd70fab010fb69e578514fd10ee31db3b791
Elise Phan <aliceconnic@gmail.com>
2019-01-26 23:09:50 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,7 +23,15 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
   
-  
+  post '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect "pets/#{@pet.id}"
+  end
   
 
   get '/pets/:id/edit' do 
